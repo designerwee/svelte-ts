7858c6088146e0e7c5a1ee30d7a1b238c495d067
designerwee <designerwee@gmail.com>
2024-06-26 22:14:08 +0900

    init

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<style lang="postcss">
+</style>
+
+<main class="flex h-screen items-center justify-center">
+	<h4>Svelte</h4>
+</main>
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './app.css';
+import App from './App.svelte';
+import 'remixicon/fonts/remixicon.css';
+
+const app = new App({
+	target: document.getElementById('app')!,
+});
+
+export default app;
